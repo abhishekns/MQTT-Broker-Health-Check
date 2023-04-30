ac2590746c9685eaf53fddfe40cc30a081c2b634
Abhishek <abhishekn.sharma@gmail.com>
2023-04-30 07:36:44 +0530

    Update publish.js

--- publish.js
+++ publish.js
@@ -2,7 +2,7 @@ const mqtt = require('mqtt');
 const INTERVAL = 100;
 
 //const client = mqtt.createClient(1883, '192.168.86.116', {encoding:'utf8', clientId: 'publish', username: 'publish', password: 'secret'});
-const client = mqtt.connect({port: 1883, host: '192.168.86.116'});
+const client = mqtt.connect({port: 1883, host: 'mqtt_broker'});
 client.on('connect', function(re, err){
 		if (err) {
 			console.log(err)

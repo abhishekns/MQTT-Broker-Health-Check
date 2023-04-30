61f94d094670444aacb6c834f88f31cc957a3234
Abhishek <abhishekn.sharma@gmail.com>
2023-04-30 07:37:21 +0530

    Update subscriber.js

--- subscriber.js
+++ subscriber.js
@@ -3,7 +3,7 @@ var mqtt = require('mqtt');
 var id = process.argv.slice(2)[0];
 
 //var client = mqtt.createClient(1883, '192.168.86.116',{clean:false,encoding:'utf8', clientId: id || 'temp'});
-var client = mqtt.connect({port: 1883, host: '192.168.86.116'});
+var client = mqtt.connect({port: 1883, host: 'mqtt_broker'});
 client.on('connect', function(re, err){
 		if (err) {
 			console.log(err)
